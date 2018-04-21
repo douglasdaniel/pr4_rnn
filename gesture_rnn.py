f63c0a74b52774cf1b526a825e2e2e3a9ec45d3f
daniel <daniel.douglas@temple.edu>
2018-04-21 14:22:40 -0400

    implemented training

--- gesture_rnn.py
+++ gesture_rnn.py
@@ -1,1 +1,99 @@
 #!/usr/bin/env python3
+
+import pickle
+import pdb
+import numpy as np
+import tensorflow as tf
+from model import model, accuracy
+
+
+DISPLAY_STEP = 100
+MAX_ITERATIONS = 10001
+N_TRAIN_SEQ = 7900
+N_TEST_SEQ = 100
+batch_size = 5
+LEARN_RATE = 1e-3
+
+
+# load the dataset into memory
+with open('./data/youtube_train_data.pkl','rb') as data_file:
+    data,labels = pickle.load(data_file)
+
+data = data.astype(np.float32)
+labels = labels.astype(np.float32)
+
+# normalize the training data over each image
+data -= np.mean(data, axis=(2,3,4), keepdims=True)
+data /= np.std(data, axis=(2,3,4), keepdims=True)
+
+# Split dataset into training and testing
+train_data = data[:N_TRAIN_SEQ]
+train_labels = labels[:N_TRAIN_SEQ]
+test_data = data[-N_TEST_SEQ:]
+test_labels = labels[-N_TEST_SEQ:]
+
+
+# Instantiate the graph
+graph = tf.Graph()
+with graph.as_default():
+
+    # Input data
+    tf_train_data = tf.placeholder(shape=[batch_size, 10, 64, 64, 3], dtype=tf.float32,name='traindata')
+    tf_train_labels = tf.placeholder(shape=[batch_size, 10, 7, 2], dtype=tf.float32,name='trainlabels')
+    tf_test_data = tf.constant(test_data,dtype=tf.float32)
+
+    # Compute predictions using the model
+    predict_op= model(tf_train_data)
+    test_predictions= model(tf_test_data)
+
+    # Compute the loss
+    loss = tf.losses.mean_squared_error(labels=tf_train_labels,predictions=predict_op)
+    
+    # Instantiate optimizer to calculate gradients
+    optimizer = tf.train.AdamOptimizer(learning_rate=LEARN_RATE).minimize(loss)
+    init_op = tf.global_variables_initializer()
+    
+    # Save the model
+    tf.get_collection('validation_nodes')
+
+    # Add opts
+    tf.add_to_collection('validation_nodes', tf_train_data)
+    tf.add_to_collection('validation_nodes', tf_train_labels)
+    tf.add_to_collection('validation_nodes', predict_op)
+
+    # Begin training
+    saver = tf.train.Saver()
+
+with tf.Session(graph=graph) as session:
+    session.run(init_op)
+    print('Initialized')
+    for step in np.arange(MAX_ITERATIONS):  
+        # Get new batch of data
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        data_batch = train_data[offset:(offset+batch_size),:]
+        label_batch = train_labels[offset:(offset+batch_size),:]
+
+        # Update input data
+        feed_dict = {tf_train_data : data_batch, tf_train_labels : label_batch}
+         
+        # Run training
+        _, lass, predictions = session.run([optimizer, loss, predict_op],feed_dict=feed_dict)
+
+        if step % DISPLAY_STEP == 0:
+            train_accuracy = accuracy(predictions, label_batch)
+            test_accuracy = accuracy(test_predictions.eval(), test_labels)
+            message = "step {:4d} : loss is {:6.2f}, training accuracy= {:2.2f} %, testing accuracy= {:2.2f} %".format(step, lass, train_accuracy, test_accuracy)
+            print(message)
+
+    save_path = saver.save(session, "./my_model")
+
+
+
+
+
+
+
+
+
+        
+
---
+++ model.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+import pdb
+import numpy as np
+
+LEARN_RATE = 1e-3
+num_units = 5
+seq_length = 10
+
+
+def model(data):
+    batch_size = data.shape[0]
+    
+    cnn_output = []
+
+    # Instantiate a LSTM cell
+    lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units,reuse=tf.AUTO_REUSE)
+
+    for sequence in tf.unstack(data, batch_size):
+        input_layer = sequence
+
+        # Convolutional Layer 1
+        conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=32,
+            kernel_size=[5,5],
+            padding="valid",
+            activation=tf.nn.relu)
+        
+        # Pooling Layer #1
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=2)
+        
+        # Convolutional Layer 2
+        conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=32,
+            kernel_size=[5, 5],
+            padding="valid",
+            activation=tf.nn.relu)
+        
+        # Pooling Layer #2
+        pool2 = tf.layers.max_pooling2d(inputs=conv2,pool_size=[3,3],strides=2)
+        
+        # Convolutional Layer 3
+        conv3 = tf.layers.conv2d(
+            inputs=pool2,
+            filters=64,
+            kernel_size=[5, 5],
+            padding="valid",
+            activation = tf.nn.relu)
+        
+        # Pooling Layer 3
+        pool3 = tf.layers.max_pooling2d(inputs=conv3,pool_size=[3,3],strides=2)
+        
+        # Fully-Connected Layer
+        fulcon = tf.reshape(pool3, [-1, pool3.shape[1]*pool3.shape[2]*64])
+
+        cnn_output.append(fulcon)
+
+    cnn_output = tf.stack(cnn_output)
+    rnn_input = cnn_output
+        
+    # Define RNN network
+    h_val, _ = tf.nn.dynamic_rnn(lstm_cell, rnn_input, dtype=tf.float32)
+
+    # Collection of all the final output
+    final_output = tf.zeros(shape=[batch_size, 0, 14])
+    w_fc  = tf.Variable(tf.random_normal([num_units,14]))
+    b_fc = tf.Variable(tf.random_normal([14]))
+    for i in np.arange(seq_length):
+        temp = tf.reshape(h_val[:,i,:], [batch_size, num_units])
+        output = tf.matmul(temp, w_fc) + b_fc
+        output = tf.reshape(output, [-1,1,14])
+        final_output = tf.concat([final_output, output], axis=1)
+    final_output = tf.reshape(final_output, [-1,10,7,2])
+        
+    return final_output
+
+def accuracy(predicts, labels):
+    diff = tf.subtract(predicts,labels)
+    acc_rat = tf.divide(diff,labels)
+    accurate = tf.reduce_mean(acc_rat)
+    
+    return accurate
+
+
